170a96419a9e6a0bbdcb969213a5aa8fb5c337e2
Andrei Calazans <andrei.calazans@coinbase.com>
2023-06-18 10:24:04 -0300

    run_example_with_fake_async

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,3 +1,5 @@
+use tokio::time::{timeout, Duration, sleep};
+use chrono::Local;
 
 async fn process_field(field: i32) {
     println!("Processing field: {:?}", field);
@@ -19,11 +21,38 @@ async fn run_example_if_let() {
     println!("DONE!");
 }
 
+
+async fn run_example_with_fake_async() {
+    println!("Fake async start: {}", Local::now());
+    /*
+     * Attention that the timeout function is here is not like in JavaScript's setTimeout. It
+     * actually refers to the limit of time that this async block can run.
+     *
+     * For a setTimeout like behavior you want to use sleep.
+     *
+     * */
+    let timeout_duration = Duration::from_secs(2);
+    if let Ok(Some(value)) = timeout(timeout_duration, async { 
+        sleep(Duration::from_secs(1)).await;
+        Some(2) 
+    }).await {
+        println!("Value: {:?}", value);
+        println!("Done at {}", Local::now());
+    } else {
+        println!("Timed out!");
+    }
+}
+
 #[tokio::main]
 async fn main() {
     /*
      * Example of using if let with async/await.
      * */
     run_example_if_let().await;
+
+    /*
+     * Example with fake async function
+     * */
+    run_example_with_fake_async().await;
 }
 
